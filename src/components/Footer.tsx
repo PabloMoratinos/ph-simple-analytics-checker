73e7e30508013a0ec3b16a50ce67da4992a3943c
Pablo Moratinos <tatfoto@gmail.com>
2025-03-28 23:09:37 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="mt-auto p-4 text-center text-xs text-analytics-gray">
-      <p>This extension analyzes the source code of the current page to detect GTM, GA4, Adobe, and Amplitude scripts.</p>
+      <p>This extension analyzes the current page to detect GTM, GA4, Adobe, Amplitude and Clarity scripts.</p>
       <p className="mt-2 font-medium">Developed by Pablo Moratinos at Product Hackers</p>
     </footer>
   );
